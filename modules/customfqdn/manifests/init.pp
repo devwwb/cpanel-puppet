68b48777dcd9815fe749d655fc102c55e7dda71b
sergio <sergio@wwb.cc>
2018-06-05 12:29:26 +0200

    Check facts in customfqdn module

--- modules/customfqdn/manifests/init.pp
+++ modules/customfqdn/manifests/init.pp
@@ -5,12 +5,14 @@ class customfqdn (
   validate_bool($enabled)
 
   if $enabled {
-     
-    file_line{'change fqdn':
-      ensure => present,
-      path   => '/etc/hosts',
-      line   => "${::ipaddress} ${::hostname}.${::fqdn_domain} ${::hostname}",
-      match  => ".*${::hostname}.${::fqdn_domain_old}.*${::hostname}.*$",
+
+    if defined('$::fqdn_domain_old') and defined('$::fqdn_domain'){
+      file_line{'change fqdn':
+        ensure => present,
+        path   => '/etc/hosts',
+        line   => "${::ipaddress} ${::hostname}.${::fqdn_domain} ${::hostname}",
+        match  => ".*${::hostname}.${::fqdn_domain_old}.*${::hostname}.*$",
+      }
     }
 
   }
