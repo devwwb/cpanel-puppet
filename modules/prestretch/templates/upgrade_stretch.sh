04d4124e0d268fe7e78854298eb214d06d2c840b
sergio <dev@wwb.cc>
2019-01-23 12:50:32 +0100

    Remove old root my.cnf

--- modules/prestretch/templates/upgrade_stretch.sh
+++ modules/prestretch/templates/upgrade_stretch.sh
@@ -4,6 +4,11 @@
 apt-get update
 apt list --upgradable
 apt-get upgrade -y
+
+#delete root /.my.conf
+rm /root/.my.cnf
+
+#dist-upgrade stretch
 apt-get dist-upgrade -y
 
 #TODO, check exit code of dpkg commands
