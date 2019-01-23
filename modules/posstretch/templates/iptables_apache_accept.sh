4dd0e630c44a53e6d9ee3addd0084e83d4f41b8c
sergio <dev@wwb.cc>
2019-01-23 14:01:12 +0100

    Save iptables rules while upgrading, and delete when finished

--- modules/posstretch/templates/iptables_apache_accept.sh
+++ modules/posstretch/templates/iptables_apache_accept.sh
@@ -4,6 +4,9 @@
 iptables -D INPUT -p tcp --dport 443 -j DROP
 iptables -D INPUT -p tcp --dport 80 -j DROP
 
+#delete temporary rules
+rm /etc/iptables/rules.v4
+
 #list iptables
 iptables -L
 
--- modules/prestretch/templates/iptables_apache_drop.sh
+++ modules/prestretch/templates/iptables_apache_drop.sh
@@ -4,6 +4,9 @@
 iptables -A INPUT -p tcp --dport 443 -j DROP
 iptables -A INPUT -p tcp --dport 80 -j DROP
 
+#save iptables for next reboot
+iptables-save > /etc/iptables/rules.v4
+
 #list iptables
 iptables -L
 
