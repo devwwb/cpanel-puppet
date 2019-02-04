0078d65cfbcbbe364eff599535b7133aeac58dfd
sergio <dev@wwb.cc>
2019-02-04 14:42:59 +0100

    Add acpid dependency to one-context

--- modules/prestretch/templates/update_onecontext.sh
+++ modules/prestretch/templates/update_onecontext.sh
@@ -6,7 +6,7 @@ echo "## Update onecontext #####################################################
 wget https://github.com/OpenNebula/addon-context-linux/releases/download/v5.6.0/one-context_5.6.0-1.deb -O /tmp/one-context_5.6.0-1.deb
 
 #install dependencies
-apt-get install curl parted open-vm-tools qemu-guest-agent libcurl3 -y
+apt-get install acpid curl parted open-vm-tools qemu-guest-agent libcurl3 -y
 
 #update one-context
 dpkg -i /tmp/one-context_5.6.0-1.deb
