a19b7b830a261d9200ea7f4318446e822c632f81
sergio <dev@wwb.cc>
2019-02-05 18:27:13 +0100

    Send report to user also

--- modules/report/templates/send_report.sh
+++ modules/report/templates/send_report.sh
@@ -3,8 +3,15 @@
 hostname=$(hostname)
 adminmail="admin@maadix.org"
 
+#get user email
+url="ldapi://"
+basedn="dc=example,dc=tld"
+useremail=`ldapsearch -Q -Y EXTERNAL -H "$url" -b "$basedn" -s one "(&(objectclass=simpleSecurityObject)(status=active))" | awk -F ": " '$1 == "email" {print $2}'`
+
+
 #send report
 cat -v /etc/maadix/report/logs/* | mail -s "Report | Infome de ${hostname}" $adminmail
+cat -v /etc/maadix/report/logs/* | mail -s "Report | Infome de ${hostname}" $useremail
 
 #del logs
 rm /etc/maadix/report/logs/*
