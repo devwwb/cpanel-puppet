665865213edcaf0ab676991e905f368c4e915cf1
sergio <dev@wwb.cc>
2019-02-06 20:15:26 +0100

    Avoid removing owncloud packages in posstretch

--- modules/posstretch/templates/delete_jessie_kernels.sh
+++ modules/posstretch/templates/delete_jessie_kernels.sh
@@ -3,4 +3,4 @@
 echo "## Delete jessie kernels ###################################################"
 
 #delete old jessie kernels
-aptitude search '~o' -F '%p' | grep -v one-context | grep -v puppet-agent | xargs apt-get -y remove --purge
+aptitude search '~o' -F '%p' | grep -v one-context | grep -v puppet-agent | grep -v owncloud | xargs apt-get -y remove --purge
