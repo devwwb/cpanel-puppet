587f9b58613703420f3374dfa89a27a093166d36
sergio <dev@wwb.cc>
2019-10-30 12:45:44 +0100

    Disable user namespaces for spotify/docker-gc

--- modules/clean/manifests/init.pp
+++ modules/clean/manifests/init.pp
@@ -15,7 +15,7 @@ class clean (
     #https://github.com/spotify/docker-gc
     if ($::docker_group){
       exec { 'clean docker':
-        command   => '/usr/bin/docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc -e GRACE_PERIOD_SECONDS=10 spotify/docker-gc',
+        command   => '/usr/bin/docker run --rm --userns host -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc -e GRACE_PERIOD_SECONDS=10 spotify/docker-gc',
         logoutput => true,
       }
     }
