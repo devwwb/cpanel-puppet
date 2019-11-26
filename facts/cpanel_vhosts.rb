b3c594cc30a19e44b300b008b2e70f5c9ef2a22b
sergio <dev@wwb.cc>
2019-11-26 09:52:20 +0100

    Add nodoby users support to domains

--- facts/cpanel_vhosts.rb
+++ facts/cpanel_vhosts.rb
@@ -21,8 +21,12 @@ Facter.add(:cpanel_vhosts, :type => :aggregate ) do
 
     Facter.value(:cpanel_domains).each do |domain, value|
       webmaster=Facter::Util::Resolution.exec('ldapsearch -H ldapi:// -Y EXTERNAL -LLL -s base -b "vd=' + domain.strip + ',o=hosting,dc=example,dc=tld" "(objectClass=VirtualDomain)" | grep adminID: | sed "s|.*: \(.*\)|\1|"')
-      Facter.value(:cpanel_users)[webmaster.strip][:type]
-      vhosts[domain.strip] = {:webmaster => webmaster.strip, :webmaster_type => Facter.value(:cpanel_users)[webmaster.strip][:type]}
+      if not webmaster == 'nobody'
+        Facter.value(:cpanel_users)[webmaster.strip][:type]
+        vhosts[domain.strip] = {:webmaster => webmaster.strip, :webmaster_type => Facter.value(:cpanel_users)[webmaster.strip][:type]}
+      else
+        vhosts[domain.strip] = {:webmaster => webmaster.strip, :webmaster_type => 'nobody'}
+      end
     end
 
     vhosts
