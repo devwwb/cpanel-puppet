458fb65954731fa75820c06a3ec13d986b67b14a
sergio <sergio@wwb.cc>
2019-12-18 13:00:38 +0100

    Include in facter cpanel_vhosts the case where sudo user is the webmaster

--- facts/cpanel_vhosts.rb
+++ facts/cpanel_vhosts.rb
@@ -18,14 +18,18 @@ Facter.add(:cpanel_vhosts, :type => :aggregate ) do
   #webmaster and webmaster_type
   chunk(:webmaster) do
     vhosts = {}
-
+    #get ssh user uid
+    adminuid = Facter::Util::Resolution.exec('ldapsearch -H ldapi:// -Y EXTERNAL -LLL -s one -b "ou=sshd,ou=People,dc=example,dc=tld" "(&(objectClass=person)(uid=*)(gidnumber=27))" | grep uid: | sed "s|.*: \(.*\)|\1|"')
     Facter.value(:cpanel_domains).each do |domain, value|
       webmaster=Facter::Util::Resolution.exec('ldapsearch -H ldapi:// -Y EXTERNAL -LLL -s base -b "vd=' + domain.strip + ',o=hosting,dc=example,dc=tld" "(objectClass=VirtualDomain)" | grep adminID: | sed "s|.*: \(.*\)|\1|"')
-      if not webmaster == 'nobody'
-        Facter.value(:cpanel_users)[webmaster.strip][:type]
+      if not webmaster == 'nobody' and not webmaster == adminuid
         vhosts[domain.strip] = {:webmaster => webmaster.strip, :webmaster_type => Facter.value(:cpanel_users)[webmaster.strip][:type]}
       else
-        vhosts[domain.strip] = {:webmaster => webmaster.strip, :webmaster_type => 'nobody'}
+        if webmaster == 'nobody'
+          vhosts[domain.strip] = {:webmaster => webmaster.strip, :webmaster_type => 'nobody'}
+        else
+          vhosts[domain.strip] = {:webmaster => webmaster.strip, :webmaster_type => 'sudo'}
+        end
       end
     end
 
@@ -35,3 +39,4 @@ Facter.add(:cpanel_vhosts, :type => :aggregate ) do
   #todo, add extra vhosts options to parse in the template file
 
 end
+
