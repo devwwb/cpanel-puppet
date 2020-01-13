e155abf3b3f24bceb852ea33dec2a505397fffdc
sergio <dev@wwb.cc>
2020-01-13 12:17:06 +0100

    Fix installing mongodb-org-tools in prestretch

--- modules/prestretch/templates/update_mongodb_34.sh
+++ modules/prestretch/templates/update_mongodb_34.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 echo "## Backup all mongodb databases ############################################"
-echo "Acquire::Check-Valid-Until 'false';" > /etc/apt/apt.conf.d/90ignore-release-date &&
-apt-get update &&
-apt-get install mongodb-org-tools -y &&
-rm /etc/apt/apt.conf.d/90ignore-release-date
+apt-get install mongodb-org-tools -y --force-yes
 DATE=`date +%Y-%m-%d`
 if [ ! -d /etc/maadix/backups ]; then
   mkdir /etc/maadix/backups
