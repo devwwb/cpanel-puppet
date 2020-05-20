0f85c88fe4ddd9c3526644f714447538bf5d99b0
sergio <sergio@wwb.cc>
2020-05-20 18:34:25 +0200

    Set ou=reboot status to readybefore rebooting

--- modules/reboot/manifests/init.pp
+++ modules/reboot/manifests/init.pp
@@ -5,6 +5,14 @@ class reboot (
   validate_bool($enabled)
 
   if $enabled {
+    #set module reboot to ready
+    ldapdn{'set ou=reboot,ou=cpanel,dc=example,dc=tld status=ready':
+      dn                  => 'ou=reboot,ou=cpanel,dc=example,dc=tld',
+      attributes          => ["status: ready"],
+      unique_attributes   => ["status"],
+      ensure              => present
+    }
+
     exec { 'reboot':
       command => "/sbin/reboot",
     }
