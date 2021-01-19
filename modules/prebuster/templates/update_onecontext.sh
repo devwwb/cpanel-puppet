5bbc100a63429b012a526cd9427006831ee7e908
sergio <sergio@tejido.io>
2021-01-19 20:57:34 +0100

    Prebuster: fix one_context script

--- modules/prebuster/templates/update_onecontext.sh
+++ modules/prebuster/templates/update_onecontext.sh
@@ -9,7 +9,7 @@ apt-get remove --purge one-context -y
 wget https://github.com/OpenNebula/addon-context-linux/releases/download/v5.12.0.1/one-context_5.12.0.1-1.deb -O /tmp/one-context_5.12.0.1-1.deb
 
 #install dependencies
-apt-get install acpid curl parted open-vm-tools qemu-guest-agent libcurl3 -y
+apt-get install acpid curl parted open-vm-tools qemu-guest-agent libcurl4 -y
 
 #update one-context
 dpkg -i /tmp/one-context_5.12.0.1-1.deb
