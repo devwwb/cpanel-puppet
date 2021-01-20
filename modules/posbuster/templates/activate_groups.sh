eb78857321b01b3e655a65e3093d63a2694a0c55
sergio <sergio@tejido.io>
2021-01-20 13:29:54 +0100

    Minor fix

--- modules/posbuster/templates/activate_groups.sh
+++ modules/posbuster/templates/activate_groups.sh
@@ -2,12 +2,12 @@
 
 echo "## Activate all groups #####################################################"
 
-#get prestretch enabled groups, excluding mail, mongo, nodejs, docker
+#get prebuster enabled groups, excluding mail, mongo, nodejs, docker
 egroups=($(ldapsearch -H ldapi:// -Y EXTERNAL -LLL -s one -b "ou=groups,dc=example,dc=tld" "(&(objectClass=*)(status=disabled)(type=upgrade))" | grep ou: | sed "s|.*: \(.*\)|\1|"))
 #get deactivated groups, excluding mail, mongo, nodejs, docker
 dgroups=($(ldapsearch -H ldapi:// -Y EXTERNAL -LLL -s one -b "ou=groups,dc=example,dc=tld" "(&(objectClass=*)(status=disabled)(type=installed))" | grep ou: | sed "s|.*: \(.*\)|\1|"))
 
-#activate prestretch enabled groups
+#activate prebuster enabled groups
 for i in "${egroups[@]}"
 do
 echo "dn: ou=$i,ou=groups,dc=example,dc=tld
