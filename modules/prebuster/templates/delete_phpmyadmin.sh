18bca2d1a393704d72cd6f6653fb16945b7611cd
sergio <sergio@tejido.io>
2021-01-20 11:18:33 +0100

    Prebuster: fix delete_phpmyadmin script

--- modules/prebuster/templates/delete_phpmyadmin.sh
+++ modules/prebuster/templates/delete_phpmyadmin.sh
@@ -4,6 +4,8 @@ echo "## Delete phpmyadmin #####################################################
 #delete phpmyadmin
 mysql --defaults-extra-file=/root/.my.cnf -e "drop database phpmyadmin;"
 mysql --defaults-extra-file=/root/.my.cnf -e "drop user phpmyadmin@localhost;"
-apt-get remove --purge phpmyadmin -y
+if apt-show-versions | grep phpmyadmin; then
+  apt-get remove --purge phpmyadmin -y
+fi
 
 
