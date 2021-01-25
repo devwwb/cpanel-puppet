8646e2191f10ae54b4a444c005d2a7b97ec3b3b8
sergio <sergio@tejido.io>
2021-01-25 19:37:51 +0100

    Posbuster: add jitsi packages as exception when removing obsolete packages

--- modules/posbuster/templates/delete_obsolete_packages.sh
+++ modules/posbuster/templates/delete_obsolete_packages.sh
@@ -6,4 +6,4 @@ apt-get install aptitude -y
 
 #remove obsolete packages
 aptitude update
-aptitude search '~o' -F '%p' | grep -v one-context | grep -v puppet-agent | xargs apt-get -y remove --purge
+aptitude search '~o' -F '%p' | grep -v one-context | grep -v puppet-agent | grep -v heirloom-mailx | grep -v jicofo | grep -v jitsi-meet | grep -v jitsi-videobridge2 | xargs apt-get -y remove --purge
