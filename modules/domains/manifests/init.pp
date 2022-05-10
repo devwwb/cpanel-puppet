db5714a97262c0e6a6716e07c1d70ccc2439824c
sergio <sergio@tejido.io>
2022-05-10 18:38:24 +0200

    Minor fix

--- modules/domains/manifests/init.pp
+++ modules/domains/manifests/init.pp
@@ -68,6 +68,7 @@ class domains (
     #clean php sessions
     Exec {'clean php sessions':
       command     => 'rm /var/lib/php/sessions/*',
+      path        => ['/usr/bin', '/bin'],
       require     => Exec['reload apache end'],
       refreshonly => true,
     }
