e541c9962157cbac92cc32208645c33626fea66f
sergio <sergio@tejido.io>
2022-06-28 12:00:35 +0200

    Domains: reset wordpress installation data and notify, when webroot not empty

--- modules/domains/templates/wp_setup.sh
+++ modules/domains/templates/wp_setup.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
 
-#get params
+#params
 DOMAIN=$1
 WEBROOT="/var/www/html/$1"
 VHOSTUSER=$2
 VHOSTGROUP=$3
-basedn="vd=$1,o=hosting,dc=example,dc=tld"
+BASEDN="vd=$1,o=hosting,dc=example,dc=tld"
+DBPASS=`pwgen -c -1 20`
+ADMINMAIL=`ldapsearch -Q -Y EXTERNAL -H ldapi:// -b 'dc=example,dc=tld' -s one "(&(objectclass=simpleSecurityObject)(status=active))" | awk -F ": " '$1 == "email" {print $2}'`
+ADMINUSER=`ldapsearch -Q -Y EXTERNAL -H ldapi:// -b 'dc=example,dc=tld' -s one "(&(objectclass=simpleSecurityObject)(status=active))" | awk -F ": " '$1 == "cn" {print $2}'`
+
+#failed mail params
+SUBJECT="Maadix: Error instalación Wordpress / Wordpress install failed"
+read -r -d '' BODY << EOM
+[ES] English below
+
+Hola,
+La instalación del Worpdress en el dominio $DOMAIN no se ha podido completar debido a que la carpeta /var/www/html/$DOMAIN contiene archivos. Para evitar perdida de datos se ha interrumpido el proceso.
+
+Para poder instalar Wordpress asegúrate que la carpeta /var/www/html/$DOMAIN esté vacía borrando o moviendo los archivos que contiene y luego vuelve a lanzar la instalación.
+
+Tu Sistema Automatizado
+
+[EN]
+
+Hello,
+The Worpdress installation at $DOMAIN could not be completed due to the fact that the /var/www/html/$DOMAIN folder contains files. To avoid data loss the process has been interrupted.
+
+To be able to install Wordpress make sure that the /var/www/html/$DOMAIN folder is empty by deleting or moving the files it contains and then relaunch the installation.
+
+Your Automated System
+
+EOM
 
 #check pass is present
-ldapsearch -o ldif-wrap=no -H ldapi:// -Y EXTERNAL -s base -b "ou=password,ou=cms,$basedn" | grep status:
+ldapsearch -o ldif-wrap=no -H ldapi:// -Y EXTERNAL -s base -b "ou=password,ou=cms,$BASEDN" | grep status:
 if [ $? -eq 0 ]; then
   #read admin user password from ldap
   #try to decrypt password if gnupg kit is setup
   if [[ -d /usr/share/mxcp/.gnupg ]]
   then
-    ADMINPASS=$(ldapsearch -o ldif-wrap=no -H ldapi:// -Y EXTERNAL -s base -b "ou=password,ou=cms,$basedn" | grep status: | sed "s|.*: \(.*\)|\1|" | base64 -d | gpg --decrypt)
+    ADMINPASS=$(ldapsearch -o ldif-wrap=no -H ldapi:// -Y EXTERNAL -s base -b "ou=password,ou=cms,$BASEDN" | grep status: | sed "s|.*: \(.*\)|\1|" | base64 -d | gpg --decrypt)
   else
-    ADMINPASS=$(ldapsearch -o ldif-wrap=no -H ldapi:// -Y EXTERNAL -s base -b "ou=password,ou=cms,$basedn" | grep status: | sed "s|.*: \(.*\)|\1|" )
+    ADMINPASS=$(ldapsearch -o ldif-wrap=no -H ldapi:// -Y EXTERNAL -s base -b "ou=password,ou=cms,$BASEDN" | grep status: | sed "s|.*: \(.*\)|\1|" )
   fi
 else
   #no password, exit
@@ -24,9 +50,9 @@ else
 fi
 
 #check dbname is present
-ldapsearch -o ldif-wrap=no -H ldapi:// -Y EXTERNAL -s base -b "ou=cms,$basedn" | grep note:
+ldapsearch -o ldif-wrap=no -H ldapi:// -Y EXTERNAL -s base -b "ou=cms,$BASEDN" | grep note:
 if [ $? -eq 0 ]; then
-  DBNAME=$(ldapsearch -o ldif-wrap=no -H ldapi:// -Y EXTERNAL -s base -b "ou=cms,$basedn" | grep note: | sed "s|.*: \(.*\)|\1|" )
+  DBNAME=$(ldapsearch -o ldif-wrap=no -H ldapi:// -Y EXTERNAL -s base -b "ou=cms,$BASEDN" | grep note: | sed "s|.*: \(.*\)|\1|" )
 else
   #no db, exit
   exit 0
@@ -35,14 +61,23 @@ fi
 #check dir is empty
 DIRCOUNT=$(ls -1A $WEBROOT |wc -l)
 if [ $DIRCOUNT -ne 0 ]; then
-  #not empty, exit
+
+  #not empty, delete pass, set as failed, notify and exit
+  ldapmodify -Q -Y EXTERNAL -H ldapi:/// << EOF
+dn: ou=cms,$BASEDN
+changetype: modify
+replace: status
+status: failed
+EOF
+
+  #delete ldap password
+  ldapdelete -H ldapi:// -Y EXTERNAL "ou=password,ou=cms,$BASEDN"
+
+  #send mail
+  echo -e "$BODY" | mail -s "$SUBJECT" $ADMINMAIL
+
   exit 0
 fi
-
-ADMINMAIL=`ldapsearch -Q -Y EXTERNAL -H ldapi:// -b 'dc=example,dc=tld' -s one "(&(objectclass=simpleSecurityObject)(status=active))" | awk -F ": " '$1 == "email" {print $2}'`
-ADMINUSER=`ldapsearch -Q -Y EXTERNAL -H ldapi:// -b 'dc=example,dc=tld' -s one "(&(objectclass=simpleSecurityObject)(status=active))" | awk -F ": " '$1 == "cn" {print $2}'`
-DBPASS=`pwgen -c -1 20`
-
 
 #create database
 if [ -f /root/.my.cnf ]; then
@@ -81,14 +116,14 @@ chown -R $VHOSTUSER:$VHOSTGROUP * -R
 
 #set installed in ldap
 ldapmodify -Q -Y EXTERNAL -H ldapi:/// << EOF
-dn: ou=cms,$basedn
+dn: ou=cms,$BASEDN
 changetype: modify
 replace: status
 status: enabled
 EOF
 
 #delete ldap password
-ldapdelete -H ldapi:// -Y EXTERNAL "ou=password,ou=cms,$basedn"
+ldapdelete -H ldapi:// -Y EXTERNAL "ou=password,ou=cms,$BASEDN"
 
 echo "WP installed with DOMAIN:$DOMAIN WEBROOT:$WEBROOT VHOSTUSER:$VHOSTUSER VHOSTGROUP:$VHOSTGROUP DBNAME:$DBNAME"
 
