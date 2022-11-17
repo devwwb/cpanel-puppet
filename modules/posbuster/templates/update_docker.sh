3f56774a2368433a1c2815dcba571378022c1924
sergio <sergio@tejido.io>
2022-11-17 18:18:34 +0100

    Posbuster: change docker version

--- modules/posbuster/templates/update_docker.sh
+++ modules/posbuster/templates/update_docker.sh
@@ -3,5 +3,5 @@
 echo "## Update docker ###########################################################"
 
 #update docker
-apt-get install --reinstall docker-ce=5:19.03.2~3-0~debian-buster -y --allow-downgrades
+apt-get install --reinstall docker-ce=5:20.10.11~3-0~debian-buster -y --allow-downgrades
 service docker restart
