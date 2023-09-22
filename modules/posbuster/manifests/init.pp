d0d6dddee3fa88b29843514e30bdc6e70fb42c75
sergio <sergio@tejido.io>
2023-09-22 11:45:53 +0200

    Posbuster: disable updating vm data in api v1

--- modules/posbuster/manifests/init.pp
+++ modules/posbuster/manifests/init.pp
@@ -167,11 +167,11 @@ class posbuster (
       # --test option implies --detailed-exitcodes. and Exitcode of 2 means that The run succeeded, and some resources were changed
       returns   => 2,
       timeout   => 7200,
-    } ->
-    exec { 'set_ready_api.sh':
-      command   => "/bin/bash -c '$directory/set_ready_api.sh >> $directory/logs/posbuster 2>&1'",
-      logoutput => true,
     }
+    #exec { 'set_ready_api.sh':
+    #  command   => "/bin/bash -c '$directory/set_ready_api.sh >> $directory/logs/posbuster 2>&1'",
+    #  logoutput => true,
+    #}
 
 
     #clean unused images and containers
@@ -203,9 +203,9 @@ class posbuster (
     exec { 'send posbuster notify':
       command   => "/bin/bash -c '$directory/send_posbuster_notify.sh'",
       logoutput => true,
-      require   =>[
-                  Exec['set_ready_api.sh'],
-                  ],
+      #require   =>[
+      #            Exec['set_ready_api.sh'],
+      #            ],
     } ->
     exec { 'enable setreadycpanel':
       command   => '/bin/systemctl enable setreadycpanel',
