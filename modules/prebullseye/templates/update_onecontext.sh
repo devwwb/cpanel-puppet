53d13be1bab3144465bd821a87ae4ee9549a904e
sergio <sergio@tejido.io>
2023-11-06 18:55:11 +0100

    Bullseye, add missing dependency of onecontext

--- modules/prebullseye/templates/update_onecontext.sh
+++ modules/prebullseye/templates/update_onecontext.sh
@@ -9,7 +9,7 @@ apt-get remove --purge one-context -y
 wget https://github.com/OpenNebula/addon-context-linux/releases/download/v6.4.0/one-context_6.4.0-1.deb -O /tmp/one-context_6.4.0-1.deb
 
 #install dependencies
-apt-get install acpid curl parted open-vm-tools qemu-guest-agent libcurl4 -y
+apt-get install acpid curl parted open-vm-tools qemu-guest-agent libcurl4 virt-what -y
 
 #update one-context
 dpkg -i /tmp/one-context_6.4.0-1.deb
