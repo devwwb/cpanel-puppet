2ae86ceab967a7803b09b65a3991810cc9e31977
sergio <sergio@tejido.io>
2023-11-07 12:32:47 +0100

    Report: list docker volumes

--- modules/report/templates/vm_docker_report.sh
+++ modules/report/templates/vm_docker_report.sh
@@ -7,3 +7,7 @@ docker container ls
 ##List docker images
 echo "## Docker images ###########################################################"
 docker images
+
+##List docker volumes
+echo "## Docker volumes ###########################################################"
+docker volume ls
