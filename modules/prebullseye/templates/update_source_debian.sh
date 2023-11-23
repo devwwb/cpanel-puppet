f2438ad3e34fa602cf45d78ea8f3cf5cc52efe94
sergio <sergio@tejido.io>
2023-11-23 13:30:32 +0100

    Bullseye: disable backports when upgrading

--- modules/prebullseye/templates/update_source_debian.sh
+++ modules/prebullseye/templates/update_source_debian.sh
@@ -8,9 +8,7 @@ deb-src http://deb.debian.org/debian bullseye main
 deb http://security.debian.org/debian-security bullseye-security main
 deb-src http://security.debian.org/debian-security bullseye-security main
 deb http://deb.debian.org/debian bullseye-updates main
-deb-src http://deb.debian.org/debian bullseye-updates main
-deb http://deb.debian.org/debian bullseye-backports main
-deb-src http://deb.debian.org/debian bullseye-backports main" > /etc/apt/sources.list
+deb-src http://deb.debian.org/debian bullseye-updates main" > /etc/apt/sources.list
 
 #delete backports
 rm /etc/apt/sources.list.d/backports.list
