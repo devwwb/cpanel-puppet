30648111d7f385c44473c02aaf0b48b16b611e15
sergio <sergio@tejido.io>
2023-11-28 17:39:52 +0100

    Fix public_ip fact

--- facts/public_ip.rb
+++ facts/public_ip.rb
@@ -4,9 +4,15 @@ require 'socket'
 
 Facter.add(:public_ip) do
   setcode do
-    fqdn = Addrinfo.getaddrinfo(Socket.gethostname, nil).first.getnameinfo.first
-    dns = Resolv::DNS.new( :nameserver => ['127.0.0.1'] )
-    public_ip = dns.getaddress( fqdn ).to_s
+    #fqdn = Addrinfo.getaddrinfo(Socket.gethostname, nil).first.getnameinfo.first
+    fqdn = Socket.gethostbyname(Socket.gethostname).first
+    begin
+      dns = Resolv::DNS.new( :nameserver => ['127.0.0.1'] )
+      public_ip = dns.getaddress( fqdn ).to_s
+    rescue
+      dns = Resolv::DNS.new( :nameserver => ['1.1.1.1'] )
+      public_ip = dns.getaddress( fqdn ).to_s
+    end
   end
 end
 
