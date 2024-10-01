faca3cca62335f3184af5c9a3f824d3cc17e2f22
sergio <sergio@tejido.io>
2024-10-01 10:36:36 +0200

    Fix deactivate_groups_and_run_puppet script

--- modules/prebullseye/templates/deactivate_groups_and_run_puppet.sh
+++ modules/prebullseye/templates/deactivate_groups_and_run_puppet.sh
@@ -23,25 +23,19 @@ status: disabled" | ldapmodify -H ldapi:// -Y EXTERNAL
 done
 
 
-#run puppet only if there were active groups
-if [ ${#egroups[@]} -eq 0 ]; then
-    echo "## All groups are disabled, puppet doesn't run #############################"
-    #exit script with 0
-    exitscript=0
+#run puppet allways
+echo "## Run puppet to update vm to latest conf without purging certs #########################################"
+/usr/bin/choom -n -1000 -- /usr/local/bin/puppet agent --certname ${hostname}.maadix.org --test --skip_tags letsencrypt::certonly,rkhunter
+# --test option implies --detailed-exitcodes. and Exitcode of 2 means that The run succeeded, and some resources were changed
+#get puppet exit code
+puppetexit=$?
+#if puppet exit is 2, the script must exit with 0, else with 1
+if [ $puppetexit -eq 2 ]; then
+  exitscript=0
 else
-    echo "## Some groups enabled, run puppet without purging certs #########################################"
-    /usr/bin/choom -n -1000 -- /usr/local/bin/puppet agent --certname ${hostname}.maadix.org --test --skip_tags letsencrypt::certonly,rkhunter
-    # --test option implies --detailed-exitcodes. and Exitcode of 2 means that The run succeeded, and some resources were changed
-    #get puppet exit code
-    puppetexit=$?
-    #if puppet exit is 2, the script must exit with 0, else with 1
-    if [ $puppetexit -eq 2 ]; then
-      exitscript=0
-    else
-      #if puppet fails, unlock cpanel and exit 1
-      /etc/init.d/setreadycpanel start
-      exitscript=1
-    fi
+  #if puppet fails, unlock cpanel and exit 1
+  /etc/init.d/setreadycpanel start
+  exitscript=1
 fi
 
 exit $exitscript
