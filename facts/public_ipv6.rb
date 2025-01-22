cec81c55a89b9e8407be922ab8540d490cad850a
sergio <sergio@tejido.io>
2025-01-22 12:36:13 +0100

    Ipv6: use expanded version

--- facts/public_ipv6.rb
+++ facts/public_ipv6.rb
@@ -1,6 +1,7 @@
 #get public ipv6 using resolv
 require "resolv"
 require 'socket'
+require 'ipaddress'
 
 Facter.add(:public_ipv6) do
   setcode do
@@ -8,11 +9,11 @@ Facter.add(:public_ipv6) do
     begin
       dns = Resolv::DNS.new( :nameserver => ['127.0.0.1'] )
       records = dns.getresources(fqdn, Resolv::DNS::Resource::IN::AAAA)
-      public_ipv6 = records[0].address.to_s
+      public_ipv6 = IPAddress::IPv6.expand records[0].address.to_s
     rescue
       dns = Resolv::DNS.new( :nameserver => ['1.1.1.1'] )
       records = dns.getresources(fqdn, Resolv::DNS::Resource::IN::AAAA)
-      public_ipv6 = records[0].address.to_s
+      public_ipv6 = IPAddress::IPv6.expand records[0].address.to_s
     end
   end
 end
