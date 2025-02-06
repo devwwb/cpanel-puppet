fd8b63c8e76877f5e58365d7ce1031bdbf5385c0
sergio <sergio@tejido.io>
2025-02-06 19:20:06 +0100

    Fix public_ipv6 fact

--- facts/public_ipv6.rb
+++ facts/public_ipv6.rb
@@ -9,11 +9,19 @@ Facter.add(:public_ipv6) do
     begin
       dns = Resolv::DNS.new( :nameserver => ['127.0.0.1'] )
       records = dns.getresources(fqdn, Resolv::DNS::Resource::IN::AAAA)
-      public_ipv6 = IPAddress::IPv6.expand records[0].address.to_s
+      begin
+        public_ipv6 = IPAddress::IPv6.expand records[0].address.to_s
+      rescue
+        public_ipv6 = nil
+      end
     rescue
       dns = Resolv::DNS.new( :nameserver => ['1.1.1.1'] )
       records = dns.getresources(fqdn, Resolv::DNS::Resource::IN::AAAA)
-      public_ipv6 = IPAddress::IPv6.expand records[0].address.to_s
+      begin
+        public_ipv6 = IPAddress::IPv6.expand records[0].address.to_s
+      rescue
+        public_ipv6 = nil
+      end
     end
   end
 end
