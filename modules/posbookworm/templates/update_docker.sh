f06037587547f6bce3237df4b63eb39a4f731a1d
sergio <sergio@tejido.io>
2025-04-29 12:45:45 +0200

    Bookworm: fix docker-ci package name

--- modules/posbookworm/templates/update_docker.sh
+++ modules/posbookworm/templates/update_docker.sh
@@ -4,5 +4,5 @@ set -e
 echo "## Update docker ###########################################################"
 
 #update docker
-apt-get install --reinstall docker-ce=5:28.1.1-1~debian.11~bookworm -y --allow-downgrades
+apt-get install --reinstall docker-ce=5:28.1.1-1~debian.12~bookworm -y --allow-downgrades
 service docker restart
