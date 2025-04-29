b64c306c27b6bfdc6dc9b847049c5ca933671bcb
sergio <sergio@tejido.io>
2025-04-29 13:22:48 +0200

    Bookworm: fix iptables accept rules

--- modules/posbookworm/templates/iptables_apache_accept.sh
+++ modules/posbookworm/templates/iptables_apache_accept.sh
@@ -5,19 +5,19 @@ echo "## Unblock http https ####################################################
 #unblock http and https access after upgrade
 
 #ipv4
-if ! iptables -C INPUT -p tcp --dport 443 -j DROP; then
+if iptables -C INPUT -p tcp --dport 443 -j DROP; then
   iptables -D INPUT -p tcp --dport 443 -j DROP
 fi
-if ! iptables -C INPUT -p tcp --dport 80 -j DROP; then
+if iptables -C INPUT -p tcp --dport 80 -j DROP; then
   iptables -D INPUT -p tcp --dport 80 -j DROP
 fi
 
 #ipv6
-if ! ip6tables -C INPUT -p tcp --dport 443 -j DROP; then
-  ip6tables -A INPUT -p tcp --dport 443 -j DROP
+if ip6tables -C INPUT -p tcp --dport 443 -j DROP; then
+  ip6tables -D INPUT -p tcp --dport 443 -j DROP
 fi
-if ! ip6tables -C INPUT -p tcp --dport 80 -j DROP; then
-  ip6tables -A INPUT -p tcp --dport 80 -j DROP
+if ip6tables -C INPUT -p tcp --dport 80 -j DROP; then
+  ip6tables -D INPUT -p tcp --dport 80 -j DROP
 fi
 
 #delete temporary rules
