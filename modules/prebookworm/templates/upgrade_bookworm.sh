b0198ea2ab68dd972ec6d97be8d0de63cc3b7403
sergio <sergio@tejido.io>
2025-05-01 13:06:46 +0200

    Bookworm: change upgrade_bookworm.sh

--- modules/prebookworm/templates/upgrade_bookworm.sh
+++ modules/prebookworm/templates/upgrade_bookworm.sh
@@ -10,17 +10,17 @@ export LC_ALL=C
 apt -y update
 apt-get clean
 echo "## List slapd.d #########################################################"
-ldapsearch -H ldapi:/// -Y external -s one -b 'dc=example,dc=tld'
+ldapsearch -H ldapi:/// -Y external -s base -b 'dc=example,dc=tld'
 echo "## Upgrade #########################################################"
 apt -y upgrade
 apt-get clean
 echo "## List slapd.d #########################################################"
-ldapsearch -H ldapi:/// -Y external -s one -b 'dc=example,dc=tld'
+ldapsearch -H ldapi:/// -Y external -s base -b 'dc=example,dc=tld'
 echo "## Full-upgrade #########################################################"
 apt -y full-upgrade
 apt-get clean
 echo "## List slapd.d #########################################################"
-ldapsearch -H ldapi:/// -Y external -s one -b 'dc=example,dc=tld'
+ldapsearch -H ldapi:/// -Y external -s base -b 'dc=example,dc=tld'
 echo "## List deleted packages to purge #########################################################"
 apt list '~c'
 
