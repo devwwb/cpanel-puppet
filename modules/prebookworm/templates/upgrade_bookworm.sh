80271990237211d437844fb06ccfa8e69fa373bf
sergio <sergio@tejido.io>
2025-07-08 17:36:59 +0200

    Bookworm: tune growroot fix

--- modules/prebookworm/templates/upgrade_bookworm.sh
+++ modules/prebookworm/templates/upgrade_bookworm.sh
@@ -6,20 +6,17 @@ echo "## Upgrade bookworm ######################################################
 #stop monit
 service monit stop
 
-#fix growroot
-#doc: https://serverfault.com/a/1168110
-if [ -f /usr/share/initramfs-tools/hooks/growroot ]; then
-  mv /usr/share/initramfs-tools/hooks/growroot /root/growroot
-  update-initramfs -u
-fi
-
 export LC_ALL=C
 apt -y update
 apt-get clean
 echo "## List slapd.d #########################################################"
 ldapsearch -H ldapi:/// -Y external -s base -b 'dc=example,dc=tld'
+echo "## Upgrade kernel #########################################################"
+#fix growroot
+#doc: https://serverfault.com/a/1168110
+apt install -y linux-image-amd64 cloud-initramfs-growroot initramfs-tools initramfs-tools-core || mv /usr/share/initramfs-tools/hooks/growroot /root/growroot && update-initramfs -u && apt install -y linux-image-amd64 cloud-initramfs-growroot initramfs-tools initramfs-tools-core
 echo "## Upgrade #########################################################"
-apt -y upgrade || mv /usr/share/initramfs-tools/hooks/growroot /root/growroot && update-initramfs -u && apt -y upgrade
+apt -y upgrade
 apt-get clean
 echo "## List slapd.d #########################################################"
 ldapsearch -H ldapi:/// -Y external -s base -b 'dc=example,dc=tld'
