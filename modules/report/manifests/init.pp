15a4da373d2150d8a16edf3f5831c6a0e75ce6ca
sergio <sergio@tejido.io>
2025-07-25 10:13:33 +0200

    Report: add network managers info

--- modules/report/manifests/init.pp
+++ modules/report/manifests/init.pp
@@ -97,6 +97,16 @@ class report (
       logoutput => true,
     }
 
+    exec { "network manager info":
+      command   => "/bin/bash -c 'echo \"## Network manager info ######\" >> $directory/logs/00_www.log && systemctl status NetworkManager.service >> $directory/logs/00_www.log'",
+      logoutput => true,
+    }
+
+    exec { "network systemd info":
+      command   => "/bin/bash -c 'echo \"## Network systemd info ######\" >> $directory/logs/00_www.log && systemctl status systemd-networkd.service >> $directory/logs/00_www.log'",
+      logoutput => true,
+    }
+
     exec { "redmine info":
       command   => "/bin/bash -c 'echo \"## REDMINE info ######\" >> $directory/logs/00_www.log && apt-show-versions | grep redmine >> $directory/logs/00_www.log'",
       logoutput => true,
