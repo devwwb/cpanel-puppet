45cf679e5d012c052e5c899c21dfe2f058da4b18
sergio <sergio@tejido.io>
2025-07-31 09:13:12 +0200

    Buster: update debian sources to archive

--- modules/prebuster/templates/update_source_debian.sh
+++ modules/prebuster/templates/update_source_debian.sh
@@ -3,11 +3,11 @@
 echo "## Update source debian ####################################################"
 
 #update debian sources
-echo "deb http://deb.debian.org/debian/ buster main
-deb-src http://deb.debian.org/debian/ buster main
-deb http://deb.debian.org/debian/ buster-updates main
-deb-src http://deb.debian.org/debian/ buster-updates main
-deb http://security.debian.org/debian-security buster/updates main
-deb-src http://security.debian.org/debian-security buster/updates main" > /etc/apt/sources.list
+echo "deb http://archive.debian.org/debian/ buster main
+deb-src http://archive.debian.org/debian/ buster main
+deb http://archive.debian.org/debian/ buster-updates main
+deb-src http://archive.debian.org/debian/ buster-updates main
+deb http://archive.debian.org/debian-security buster/updates main
+deb-src http://archive.debian.org/debian-security buster/updates main" > /etc/apt/sources.list
 
 cat /etc/apt/sources.list
