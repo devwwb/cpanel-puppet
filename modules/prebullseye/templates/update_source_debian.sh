b09d3b97726fd958f4e147dd7e292e043578f086
sergio <sergio@tejido.io>
2026-09-07 14:30:55 +0200

    Bullseye: use archive repos

--- modules/prebullseye/templates/update_source_debian.sh
+++ modules/prebullseye/templates/update_source_debian.sh
@@ -3,12 +3,12 @@
 echo "## Update source debian ####################################################"
 
 #update debian sources
-echo "deb http://deb.debian.org/debian bullseye main
-deb-src http://deb.debian.org/debian bullseye main
-deb http://security.debian.org/debian-security bullseye-security main
-deb-src http://security.debian.org/debian-security bullseye-security main
-deb http://deb.debian.org/debian bullseye-updates main
-deb-src http://deb.debian.org/debian bullseye-updates main" > /etc/apt/sources.list
+echo "deb http://archive.debian.org/debian bullseye main
+deb-src http://archive.debian.org/debian bullseye main
+deb http://archive.debian.org/debian-security bullseye-security main
+deb-src http://archive.debian.org/debian-security bullseye-security main
+deb http://archive.debian.org/debian bullseye-updates main
+deb-src http://archive.debian.org/debian bullseye-updates main" > /etc/apt/sources.list
 
 #delete backports
 rm /etc/apt/sources.list.d/backports.list
