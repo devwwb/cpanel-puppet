3cd1a7df70f4318e3ba61d2149417d2fb3865658
sergio <sergio@tejido.io>
2026-09-09 09:59:34 +0200

    Bullseye: use archive repos and remove security repo

--- modules/prebullseye/templates/update_source_debian.sh
+++ modules/prebullseye/templates/update_source_debian.sh
@@ -6,12 +6,11 @@ echo "## Update source debian ##################################################
 echo 'Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/90ignore-release-date
 
 #update debian sources
-echo "deb http://deb.debian.org/debian bullseye main
-deb-src http://deb.debian.org/debian bullseye main
-deb http://security.debian.org/debian-security bullseye-security main
-deb-src http://security.debian.org/debian-security bullseye-security main
-deb http://deb.debian.org/debian bullseye-updates main
-deb-src http://deb.debian.org/debian bullseye-updates main" > /etc/apt/sources.list
+#doc: https://debiansupport.com/mirrors/
+echo "deb http://archive.debian.org/debian bullseye main
+deb-src http://archive.debian.org/debian bullseye main
+deb http://archive.debian.org/debian bullseye-updates main
+deb-src http://archive.debian.org/debian bullseye-updates main" > /etc/apt/sources.list
 
 #delete backports
 rm /etc/apt/sources.list.d/backports.list
